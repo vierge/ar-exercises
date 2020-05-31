120004a4761463676f3160463a93a122ba6f6fc8
vierge <marancholia@gmail.com>
2020-05-31 23:57:16 +0000

    completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+company_revenue = Store.sum(:annual_revenue)
+avg_store_revenue = Store.average(:annual_revenue)
+stores_doing_well = Store.where("annual_revenue >= ?", 1000000).count
+
+
+puts "total company revenue: #{company_revenue}"
+puts "average store revenue: #{avg_store_revenue}"
+puts "stores generating a million or more: #{stores_doing_well}"
