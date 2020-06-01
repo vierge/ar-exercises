b42e3a499df502ec6eed1652968b94176beefd07
vierge <marancholia@gmail.com>
2020-06-01 00:41:11 +0000

    exercise 7 completed

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,29 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee
+  def valid_wage?
+    { in: 40..200 }
+  end 
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :store_id, presence: true
+  validates :hourly_wage, presence: true, if: :valid_wage? 
+end
+
+class Store
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { greater_than: 0 }
+end
+
+puts "Enter a store name:"
+@thisstore = gets.chomp.to_s
+
+new_store = Store.create(name: @thisstore)
+
+pp new_store
+
+puts "Name invalid? ::" + new_store.errors[:name].any?.to_s
+puts "Revenue invalid? ::" + new_store.errors[:annual_revenue].any?.to_s
